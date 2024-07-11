767d3a857365a5d00d2b6ced23bbb57863132281
yukioz <yukio.link@gmail.com>
2024-07-11 17:09:11 -0300

    [fix/11] - remove commentary.
    Signed-off-by: yukioz <yukio.link@gmail.com>

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const User = require('../Models/userSchema');
 const bcrypt = require('bcrypt');
 
-// const chave = "HSUQWHUIH"
 const salt = bcrypt.genSaltSync();
 
 const signUp = async (req, res) => {
