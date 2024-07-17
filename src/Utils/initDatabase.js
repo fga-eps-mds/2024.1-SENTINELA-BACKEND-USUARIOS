ce68fbe343888cfef8d84d8deec3528324ee4391
yukioz <yukio.link@gmail.com>
2024-07-17 02:15:54 -0300

    add default admn user.
    Signed-off-by: yukioz <yukio.link@gmail.com>

--- src/Utils/initDatabase.js
+++ src/Utils/initDatabase.js
@@ -1,6 +1,10 @@
 // ./utils/initRoles.js
 const mongoose = require('mongoose');
 const Role = require('../Models/roleSchema'); // Ajuste o caminho conforme necessário
+const User = require('../Models/userSchema');
+const bcrypt = require('bcrypt');
+
+const salt = bcrypt.genSaltSync();
 
 const initializeRoles = async () => {
   const roles = [
@@ -37,7 +41,7 @@ const initializeRoles = async () => {
   ];
 
   try {
-    // Certifique-se de que a conexão está aberta antes de executar
+    // Verificar se a conexão está aberta antes de executar
     if (mongoose.connection.readyState === 1) {
       for (const roleData of roles) {
         const existingRole = await Role.findOne({ name: roleData.name });
@@ -54,6 +58,42 @@ const initializeRoles = async () => {
     }
   } catch (err) {
     console.error('Error initializing roles:', err);
+  }
+
+  try {
+    // Verificar se a conexão está aberta antes de executar
+    if (mongoose.connection.readyState === 1) {
+      // Busca o user 'administrador'
+      const adminRole = await Role.findOne({ name: 'administrador' });
+      if (!adminRole) {
+        console.error('Role "administrador" não encontrada. Crie a role antes de adicionar o usuário administrador.');
+        return;
+      }
+
+      // Verifica se o usuário administrador já existe
+      const existingAdmin = await User.findOne({ email: 'admin@admin.com' });
+      if (!existingAdmin) {
+        const hashedPassword = await bcrypt.hash('senha', salt); // Altere a senha padrão conforme necessário
+
+        // Cria o usuário administrador
+        const adminUser = new User({
+          name: 'Admin',
+          email: 'admin@admin.com',
+          phone: '1234567890',
+          password: hashedPassword,
+          role: adminRole._id
+        });
+
+        await adminUser.save();
+        console.log('Usuário administrador criado com sucesso.');
+      } else {
+        console.log('Usuário administrador já existe.');
+      }
+    } else {
+      console.error('Mongoose connection is not open');
+    }
+  } catch (err) {
+    console.error('Error initializing admin user:', err);
   }
 };
 
