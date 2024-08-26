0738bde713c74212a89e860cefd68d69280f6eda
Milena Linda <milenafernandes594@gmail.com>
2024-08-26 16:16:14 -0300

    [test-99] - test of organController of 500 error
    Co-Authored-by: Diogo-Barboza <diogorodriguesbb@gmail.com>
    Signed-off-by: Milena Linda <milenafernandes594@gmail.com>

--- src/__tests__/organController.test.js
+++ src/__tests__/organController.test.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("../routes"); // Ajuste o caminho conforme necessário
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
 const app = express();
 let mongoServer;
